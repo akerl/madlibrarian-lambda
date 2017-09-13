a7addbb2b78e17ca9a6f8780d1bec292ddf2c55d
Les Aker <me@lesaker.org>
2017-09-13 01:41:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+madlibrarian-lambda
+=========
+
+[![Build Status](https://img.shields.io/circleci/project/akerl/madlibrarian-lambda/master.svg)](https://circleci.com/gh/akerl/madlibrarian-lambda)
+[![GitHub release](https://img.shields.io/github/release/akerl/madlibrarian-lambda.svg)](https://github.com/akerl/madlibrarian-lambda/releases)
+[![MIT Licensed](https://img.shields.io/badge/license-MIT-green.svg)](https://tldrlegal.com/license/mit-license)
+
+DESC
+
+## Usage
+
+## Installation
+
+## License
+
+madlibrarian-lambda is released under the MIT License. See the bundled LICENSE file for details.
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/akerl/madlibrarian/utils"
+	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net"
+	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net/apigatewayproxy"
+)
+
+// Handle is the exported handler called by AWS Lambda.
+var Handle apigatewayproxy.Handler
+
+func init() {
+	ln := net.Listen()
+	Handle = apigatewayproxy.New(ln, []string{}).Handle
+	go http.Serve(ln, http.HandlerFunc(handle))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	s, err := utils.NewStoryFromPath("https://gist.githubusercontent.com/akerl/9321889d817beaddae2b66323e6b5a18/raw/1076def51a8d9beebddce075a0d841d2197c3bfb/gistfile1.txt")
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+	}
+	q, err := s.Generate()
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+	}
+	w.Write([]byte(q))
+}
