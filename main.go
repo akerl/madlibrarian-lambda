3c01cdbeead4fdcf7b07040ca57d8d10c9a19ae7
Les Aker <me@lesaker.org>
2018-02-24 22:40:16 -0500

    implement rudimentary caching

--- main.go
+++ main.go
@@ -8,24 +8,45 @@ import (
 	"github.com/akerl/madlibrarian/utils"
 )
 
+type storySet map[string]*utils.Story
+type bucketSet map[string]storySet
+
+var cache = make(bucketSet)
+
+func cacheStory(bucketName, storyName string) (*utils.Story, error) {
+	if cache[bucketName] == nil {
+		cache[bucketName] = make(storySet)
+	}
+	if cache[bucketName][storyName] == nil {
+		storyObject := fmt.Sprintf("meta/%s.yml", storyName)
+
+		config, err := s3.GetObject(bucketName, storyObject)
+		if err != nil {
+			return &utils.Story{}, fmt.Errorf("config not found")
+		}
+
+		story, err := utils.NewStoryFromText(config)
+		if err != nil {
+			return &utils.Story{}, fmt.Errorf("failed to parse config")
+		}
+		cache[bucketName][storyName] = &story
+	}
+	return cache[bucketName][storyName], nil
+}
+
 func loadQuote(req apigw.Request, params apigw.Params) (interface{}, error) {
 	bucketName := params.Lookup("bucket")
 	storyName := params.Lookup("story")
-	storyObject := fmt.Sprintf("meta/%s.yml", storyName)
 
 	if bucketName == "" || storyName == "" {
 		return "", fmt.Errorf("settings not provided")
 	}
 
-	config, err := s3.GetObject(bucketName, storyObject)
+	story, err := cacheStory(bucketName, storyName)
 	if err != nil {
-		return "", fmt.Errorf("config not found")
+		return "", err
 	}
 
-	story, err := utils.NewStoryFromText(config)
-	if err != nil {
-		return "", fmt.Errorf("failed to parse config")
-	}
 	quote, err := story.Generate()
 	if err != nil {
 		return "", fmt.Errorf("failed to generate quote")
